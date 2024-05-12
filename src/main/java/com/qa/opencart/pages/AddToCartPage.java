f45bbf1165727e0af39897c17d2442d40ca32620
Khaja Shahiq <khajashahiq@gmail.com>
2024-05-12 14:46:35 +0530

    added addtocartpage

---
+++ src/main/java/com/qa/opencart/pages/AddToCartPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class AddToCartPage {
+
+	
+public static void main(String[] args) {	
+	int i=10;
+	System.out.println(" Demo page for add to cart");
+	
+}
+	
+}
